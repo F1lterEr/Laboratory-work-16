33aff01b4d91c7c83ec2d955b27f9806508a7dbe
F1lterEr <41ilter.er@gmail.com>
2022-05-28 10:01:55 +0300

    first commit

---
+++ Main.java
@@ -0,0 +1,56 @@
+package com.company;
+
+import java.util.Scanner;
+
+class FirstClass{
+    double a, b, c;
+
+    public FirstClass(double a, double b, double c){
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+    void toSolve(double a, double b, double c){
+        System.out.println(a * Math.pow(b, c));
+    }
+}
+
+class SecondClass{
+    double a, b, c;
+
+    public SecondClass(double a, double b, double c){
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+    void toSolve(double a, double b, double c){
+        System.out.println(a * Math.pow(b, c));
+    }
+}
+
+@FunctionalInterface
+interface Converter<First, Second>{
+    Second convert(First f);
+
+    static <First> boolean isNotNull(First f){
+        return f != null;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите число:");
+        System.out.print("A: \n");
+        double a = in.nextDouble();
+        System.out.print("B: \n");
+        double b = in.nextDouble();
+        System.out.print("C: \n");
+        double c = in.nextDouble();
+        FirstClass fc = new FirstClass(a, b, c);
+        Converter<FirstClass, SecondClass> converter = x -> new SecondClass(x.a, x.b, x.c);
+        SecondClass sc = converter.convert(fc);
+        sc.toSolve(a, b, c);
+
+    }
+}
